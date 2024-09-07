0252088e2cb120ba4d71686be529cd6ca2cd09db
user <user@MacBook-Air-user.local>
2024-09-07 17:33:49 +0300

    login page

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,36 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+  root: resolve(__dirname, 'src'),
+  css: ["@/style.css"],
   plugins: [
     handlebars({
         partialDirectory: resolve(__dirname, 'src/partials'),
         context: {
-            title: 'Hello, world!',
+            titleTest: 'Hello, world!',
+            error404: {
+                errorCode: "404",
+                errorText: "Не туда попали",
+            },
+            error500: {
+                errorCode: "500",
+                errorText: "Мы уже фиксим",
+            },
+            login: {
+                title: 'Вход',
+                fields: [
+                    {
+                        label: 'Логин',
+                        name: 'login',
+                        type: 'text',
+                    },
+                    {
+                        label: 'Пароль',
+                        name: 'password',
+                        type: 'password',
+                    }
+                ]
+            }
         },
     }),
   ],
