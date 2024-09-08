45a9829083519bffbb8c9e114a921ff893f2f932
user <user@MacBook-Air-user.local>
2024-09-08 13:43:17 +0300

    reg

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,46 @@ export default {
                         type: 'password',
                     }
                 ]
+            },
+            registration: {
+                title: 'Регистрация',
+                fields: [
+                    {
+                        label: 'Почта',
+                        name: 'email',
+                        type: 'email',
+                    },
+                    {
+                        label: 'Логин',
+                        name: 'login',
+                        type: 'text',
+                    },
+                    {
+                        label: 'Имя',
+                        name: 'first_name',
+                        type: 'text',
+                    },
+                    {
+                        label: 'Фамилия',
+                        name: 'second_name',
+                        type: 'text',
+                    },
+                    {
+                        label: 'Телефон',
+                        name: 'phone',
+                        type: 'phone',
+                    },
+                    {
+                        label: 'Пароль',
+                        name: 'password',
+                        type: 'password',
+                    },
+                    {
+                        label: 'Пароль (ещё раз)',
+                        name: 'password',
+                        type: 'password',
+                    }
+                ]
             }
         },
     }),
