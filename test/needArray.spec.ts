3e7f57153cce0156397313b8f0ef28e62304579c
doroshenko <m.doroshenko@abr.ru>
2024-10-20 13:38:13 +0300

    add mocha

---
+++ test/needArray.spec.ts
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import needArray from "../src/utils/needArray";
+
+describe("Typescript + Babel usage suite", () => {
+    it("should return string correctly", () => {
+      assert.isArray(needArray("mocha"));
+    });
+  }); 
