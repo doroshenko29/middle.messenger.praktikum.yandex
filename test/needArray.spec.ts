c02d42c35f86a2ee378823da7ae1ea64fba72d6a
doroshenko <m.doroshenko@abr.ru>
2024-10-20 14:24:35 +0300

    pre-commit test

--- test/needArray.spec.ts
+++ test/needArray.spec.ts
@@ -4,5 +4,6 @@ import needArray from "../src/utils/needArray";
 describe("Typescript + Babel usage suite", () => {
     it("should return string correctly", () => {
       assert.isArray(needArray("mocha"));
+      assert.isArray(needArray(["test"]));
     });
   }); 
